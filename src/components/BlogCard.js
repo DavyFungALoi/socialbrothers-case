ab2241d6f3c5f5c1cd831cfc4fb6535f75f11dac
Davy <davyfungaloi@gmail.com>
2021-05-12 19:38:21 +0200

    fixed some styling for the cards

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -7,11 +7,16 @@ const BlogCard = (props) => {
   const dayMonth = splitDate.slice(6);
   const year = splitDate.substring(0, 4);
   const finishedDate = `${dayMonth}-${year}`;
-  console.log(finishedDate);
 
+  const blog_card_header_style = {
+    cursor: "not-allowed",
+    backgroundImage: `url(${img})`,
+    backgroundSize: "cover",
+  };
+  console.log(img)
   return (
     <div className="blog-card">
-      <div className="blog-card__header">
+      <div className="blog-card__header" style={blog_card_header_style}>
         <div>{finishedDate}</div>
         {category ? (
           <div className="blog-card__category">{category.name}</div>
@@ -26,3 +31,5 @@ const BlogCard = (props) => {
 };
 
 export default BlogCard;
+
+///  style={{ backgroundImage: `url(${blackcupheroimage})` }}
